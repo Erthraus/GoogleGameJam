5abb677bd83365ab1341855874e7096ea303e8d6
Mert Kavilcioğlu <mertkavilci@gmail.com>
2023-04-08 13:55:37 +0300

    Added First Menu-UI

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ProjectsButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GameMenuButton()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
